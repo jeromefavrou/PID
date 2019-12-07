ebe69ad91d04cec9faa9d706370411f36855ed4a
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2019-12-07 23:18:00 +0100

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,47 +1,20 @@
 #include "PID.hpp"
 
-#define Delta_time int(5)
+//comment or uncomment for simulate or not
+#define __SIM__
+
+#define delta_time 500 //500 ms
 
 uint8_t const Ai_size{2};
 
 struct Pin{uint8_t const number;float value;};
 
-struct Pin Ai[Ai_size]{{A0,.0f},{A1,.0f}};
+struct Pin Ai[Ai_size]{{A0,.0},{A1,.0}};
 struct Pin Do{3,LOW};
 
-PID Reg_Ai;
-long int freq_reg{0};
 
 unsigned long Thread_inst{0};
 
-void setup() 
-{
-    Reg_Ai.lock_sensor(Ai[0].value);//test sur tmp0
-    Reg_Ai.lock_order(Ai[1].value);
-    Reg_Ai.parameter(Delta_time/1000.0,10,0.05,1);
-    Reg_Ai.direction(-1);
-  
-    Thread_inst=millis();
-}
-
-void loop() 
-{
-  update_Ai<Ai_size>(Ai); //lecture des analoges
-  
-  calibrate_termistor(Ai[0]); //calibrage
-  calibrate_potentiometre(Ai[1]); //calibrage
-  
-  thread_regulator<Delta_time>(Thread_inst, Reg_Ai,Do.value);
-  
-  analogWrite(Do.number,Do.value);
-
-  delay(1);
-}
-
-
-///////////////////////////////
-//////FUNCTION/////////////////
-///////////////////////////////
 
 template<uint8_t N> void update_Ai(struct Pin lst[N])
 {
@@ -74,6 +47,64 @@ template<int periode> void thread_regulator(unsigned long & time_inst, PID & _re
   { 
     time_inst=millis();
     
-    do_value =_regulator.corrector(-255,0)*-1.0; /// for direcly use do_value in analogWrite()
+    do_value =_regulator.corrector(-255,0)*-1.0;
   } 
+}
+
+PID Reg_Ai;
+long int freq_reg{0};
+
+void setup() 
+{
+    #ifdef __SIM__
+    Serial.begin(19200);
+    #endif
+  
+    Reg_Ai.lock_sensor(Ai[0].value);//test sur tmp0
+    Reg_Ai.lock_order(Ai[1].value);
+    Reg_Ai.parameter(float(delta_time)/1000.0,20,0.6,10);
+    Reg_Ai.direction(-1);
+  
+    Thread_inst=millis();
+}
+
+#ifdef __SIM__
+void simulate(float & _temp, float _pow)
+{
+  _temp+=(_pow-255.0/2.75)*(0.003)-(_temp/300.0);
+}
+
+void display_sim(float order,float _temp, float _pow)
+{
+  Serial.print(order);
+  Serial.print(",");
+  Serial.print(_temp);
+  Serial.print(",");
+  Serial.print((float(_pow)/255.0)*100.0);
+  Serial.println("");
+}
+#endif
+
+void loop() 
+{
+  
+  #ifdef __SIM__
+  simulate(Ai[0].value,Do.value);
+  Ai[1].value=55;
+  #else
+  update_Ai<Ai_size>(Ai); //lecture des analoges
+  calibrate_termistor(Ai[0]); //calibrage
+  Ai[0].value += -0 ; //etalonage
+  calibrate_potentiometre(Ai[1]); //calibrage
+  #endif
+  
+  thread_regulator<delta_time>(Thread_inst, Reg_Ai,Do.value);
+  
+  analogWrite(Do.number,Do.value);
+  
+  #ifdef __SIM__
+  display_sim(Ai[1].value,Ai[0].value,Do.value);
+  #endif
+
+  delay(50);
 }
